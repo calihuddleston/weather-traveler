b1eef5fde5324326b0ae15921c7b18354c106f99
calihuddleston <calihuddleston@gmail.com>
2022-07-07 23:42:21 -0400

    'continuing to work on javascript, and search result html display'

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,1 +1,5 @@
-let results = document.getElementById
+let results = document.getElementById('result-text');
+
+let test = document.location.search.split('&');
+let test2 = test[0].split('=').pop();
+console.log(test2);
